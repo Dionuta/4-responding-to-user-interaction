c0a8efdfa0c06f3467b870fbe547ac28cdcf04d5
Dionuta <diounta@gmail.com>
2019-01-14 20:49:44 -0800

    notes

--- 4-responding-to-user-interaction/vid-3-delaying-execution-setTimeout/start/temp.js
+++ 4-responding-to-user-interaction/vid-3-delaying-execution-setTimeout/start/temp.js
@@ -1,8 +1,4 @@
-
-function exec(func, arg) {
-  func(arg);
-}
-
-exec((something) => {
-  console.log(something);
-}, 'Greetings, everyone!');
+//The window object has a setTimeout function we can use to delay the execution of a function.
+window.setTimeout((something) => {
+  console.log(something);  
+}, 3000, 'Greetings, everyone!');
